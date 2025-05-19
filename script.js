b61e87ec39661a15d9383e89e3cf2b6784332b2a
Nuzaili-design <abal-nuzaili@stu.okan.edu.tr>
2025-05-19 22:08:22 +0300

    Updated

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  let typingEffect = new Typed(".typedText",{
-    strings : ["Designer","Developer"],
+    strings : ["Software Engineer","Data Analyst"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
